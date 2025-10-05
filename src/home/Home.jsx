1e7ae040eda210918216362856ed09f600020544
Mehrad aminsharei <mehradaminsharei@gmail.com>
2025-10-05 18:35:23 +0330

    update Hygraph endpoint and config

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,7 +4,7 @@ import Blog from "../components/Blogs/Blog";
 
 function Home() {
   return (
-    <Container maxWidth="xl" sx={{ mt: 4 }} columnSpacing={5}>
+    <Container maxWidth="xl" sx={{ mt: 4 }}>
       <Grid container spacing={2} columnSpacing={2}>
         <Grid item size={{ xs: 12, md: 3 }}>
           <Typography variant="h5" fontWeight="bold" mb={2}>
